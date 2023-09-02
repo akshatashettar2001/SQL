103d4e68b1dbea16de9f3bec8d63485025df3bce
akshatashettar2001 <akshatashettar.x-workz@gmail.com>
2023-09-03 02:19:46 +1300

    uploded

---
+++ Tasks/1-9-2023/Sept_1st.sql
@@ -0,0 +1,90 @@
+SELECT * FROM election_results;
+
+INSERT INTO election_results VALUES (1,'101','RAJU','301','23-3-2023','city_hall',500,58,'District_A','Winner');
+INSERT INTO election_results VALUES (2,'101','Ramya','304','23-3-2023','rotary_club',600,42,'District_A','Runner');
+INSERT INTO election_results VALUES (3,'101','Arjun','420','23-3-2023','lion_school',50,4,'District_A','lost');
+INSERT INTO election_results VALUES (4,'101','Suhas','403','23-3-2023','rotary_club',30,3,'District_A','lost');
+INSERT INTO election_results VALUES (5,'102','Roja','502','23-3-2023','rotary_club',510,48,'District_B','Winner');
+INSERT INTO election_results VALUES (6,'102','Soumya','535','23-3-2023','school_gym',490,46,'District_B','Runner');
+INSERT INTO election_results VALUES (7,'102','Mahesh','307','25-3-2023','community_center',10,1,'District_B','lost');
+INSERT INTO election_results VALUES (8,'102','Sandeep','402','23-3-2023','city_hall',5,0,'District_B','lost');
+INSERT INTO election_results VALUES (9,'103','Pankaja','222','23-3-2023','school_gym',480,47,'District_C','Winner');
+INSERT INTO election_results VALUES (10,'103','Nirmala','310','25-3-2023','community_center',490,48,'District_C','Runner');
+INSERT INTO election_results VALUES (11,'103','Somu','311','23-3-2023','city_hall',20,2,'District_C','lost');
+INSERT INTO election_results VALUES (12,'103','Deepak','205','23-3-2023','school_gym',10,1,'District_C','lost');
+INSERT INTO election_results VALUES (13,'104','Bheemesh','444','23-3-2023','city_central_park',600,60,'District_D','Winner');
+INSERT INTO election_results VALUES (14,'104','Jagadeesh','405','23-3-2023','city_central_park',290,29,'District_D','Runner');
+INSERT INTO election_results VALUES (15,'104','Chanchala','565','23-3-2023','city_central_park',100,10,'District_D','lost');
+INSERT INTO election_results VALUES (16,'104','Satish','261','23-3-2023','city_central_park',110,11,'District_D','lost');
+INSERT INTO election_results VALUES (17,'105','Ramesh','491','23-3-2023','city_complex',90,9,'District_E','lost');
+INSERT INTO election_results VALUES (18,'105','Rajeev','275','23-3-2023','city_complex',110,11,'District_E','lost');
+INSERT INTO election_results VALUES (19,'105','Akila','399','23-3-2023','city_complex',240,24,'District_E','Runner');
+INSERT INTO election_results VALUES (20,'105','Vanitha','316','23-3-2023','city_complex',560,56,'District_E','Winner');
+
+
+======================================================================================================================
+
+
+SELECT *FROM indian_festivals;
+
+DESC indian_festivals;
+
+INSERT INTO indian_festivals VALUES(1,'Ganesh_chaturthi','18-8-23','23-8-23','india','Hindu_fest','tradition','religious','praying ganesha','modaka');
+INSERT INTO indian_festivals VALUES(2,'Diwali','19-9-23','20-9-23','india','festival_of_lights','lighting_lamps','religious','dance_music','sweets');
+INSERT INTO indian_festivals VALUES(3,'Thanks_giving','23-11-23','14-11-23','USA','gratitude','paradise','cultural','football','roast');
+INSERT INTO indian_festivals VALUES(4,'karnival','22-9-23','25-9-23','Brazil','street_party','samba_dance','cultural','samba_paradise','blackbeenstew');
+INSERT INTO indian_festivals VALUES(5,'hanukkah','18-12-23','26-12-23','World_wide','juice_festival_of_lights','lighting_menora','religious','singing_hanukkah_songs','natkes');
+INSERT INTO indian_festivals VALUES(6,'Holi','17-3-23','18-3-23','india','festival_of_colors','throwing colors','cultural','dance_music','thandai');
+INSERT INTO indian_festivals VALUES(7,'Oktoberfest','16-9-23','3-10-23','Jarmany','beer_festival','beer drinking','cultural','dance_music','pretgel');
+INSERT INTO indian_festivals VALUES(8,'new year','22-1-23','8-2-23','China','lunar new year','dance','cultural','lateran festival','dumplinks');
+INSERT INTO indian_festivals VALUES(9,'marti grass','28-2-23','5-3-23','USA','colorful','bead throwing','cultural','jarge music','king cake');
+INSERT INTO indian_festivals VALUES(10,'Eid','1-5-23','2-5-23','world wide','islamic festival','prayering','religious','visiting family','biriyani');
+INSERT INTO indian_festivals VALUES(11,'bastille day','14-7-23','15-7-23','france','french national day','fire works','cultural','dance_music','coq au vin');
+INSERT INTO indian_festivals VALUES(12,'nagar panchami','17-8-23','18-8-23','india','festival_of_snakes','worship snake god','religious','Jokali','Ladu');
+INSERT INTO indian_festivals VALUES(13,'raksha Bandhan','31-8-23','1-8-23','india','festival_of_rakhi','taging rakhi','religious','dance_music','sweets');
+INSERT INTO indian_festivals VALUES(14,'Diwali','19-9-23','20-9-23','india','festival_of_lights','lighting_lamps','religious','dance_music','jamun');
+INSERT INTO indian_festivals VALUES(15,'MangalGowri','2-8-23','3-8-23','india','festival_of_gowri','decoratiON','religious','pooja','Obbattu');
+INSERT INTO indian_festivals VALUES(16,'Dasara','12-10-23','20-10-23','india','festival_of_goddessess','Mysore_Ambhari','religious','dance_music','sweets');
+INSERT INTO indian_festivals VALUES(17,'Shivaratri','19-2-23','20-2-23','india','festival_of_meditation','Worshiping shiva','religious','prayer','Fruit Salad');
+INSERT INTO indian_festivals VALUES(18,'onum','19-7-23','20-7-23','kerala','festival_of_keralians','cooking food','religious','dance_music','onum spcial');
+INSERT INTO indian_festivals VALUES(19,'Sankranti','14-1-23','15-1-23','india','festival_of_crop harvest','sharing stuff','religious','Dandya','Ellu-Bella');
+INSERT INTO indian_festivals VALUES(20,'Yugadi','19-3-23','20-3-23','india','indian new year','oil bath','religious','prayer','sweets');
+
+=================================================================================================================================================
+
+
+SELECT * FROM karnataka_waterfalls;
+
+DESC karnataka_waterfalls;
+
+INSERT INTO karnataka_waterfalls VALUES(1, 'Jog Falls', 'Shimoga', 253, 472, 'Sharavathi River', 'Shimoga', 'Shimoga', 50.00, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(2, 'Abbey Falls', 'Coorg', 70, 20, 'Kaveri River', 'Madikeri', 'Kodagu', 15.00, 'Year-round');
+INSERT INTO karnataka_waterfalls VALUES(3, 'Unchalli Falls', 'Siddapur', 116, 380, 'Aghanashini River', 'Siddapur', 'Uttara Kannada', 20.00, 'Monsoon');
+
+
+INSERT INTO karnataka_waterfalls VALUES(1, 'Jog Falls', 'Shimoga', 253.3, 472.4, 'Sharavati', 'Sagara', 'Shimoga', 50.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(2, 'Shivanasamudra Falls', 'Mandya', 98.7, 305.0, 'Kaveri', 'Mysore', 'Mandya', 20.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(3, 'Abbey Falls', 'Coorg', 21.3, 10.1, 'Kaveri', 'Madikeri', 'Kodagu', 15.0, 'Year-round');
+INSERT INTO karnataka_waterfalls VALUES(4, 'Unchalli Falls', 'Haveri', 116.0, 380.0, 'Aghanashini', 'Sirsi', 'Uttara Kannada', 30.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(5, 'Iruppu Falls', 'Coorg', 51.0, 24.0, 'Lakshmana Tirtha', 'Virajpet', 'Kodagu', 10.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(6, 'Dudhsagar Falls', 'Goa', 310.0, 30.0, 'Mandovi', 'Castle Rock', 'Belgaum', 100.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(7, 'Magod Falls', 'Yellapur', 650.0, 30.0, 'Bedti', 'Yellapur', 'Uttara Kannada', 20.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(8, 'Sathodi Falls', 'Yellapur', 15.0, 20.0, 'Kali', 'Yellapur', 'Uttara Kannada', 10.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(9, 'Hebbe Falls', 'Chikkamagaluru', 168.0, 10.0, 'Chikmagalur', 'Chikkamagaluru', 'Chikkamagaluru', 30.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(10, 'Lalguli Falls', 'Karwar', 250.0, 10.0, 'Kali', 'Karwar', 'Uttara Kannada', 10.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(11, 'Gokak Falls', 'Gokak', 170.0, 10.0, 'Ghataprabha', 'Gokak', 'Belgaum', 10.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(12, 'Lushington Falls', 'Shirley', 250.0, 30.0, 'Mahanadi', 'Kolagallu', 'Bellary', 10.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(13, 'Mallalli Falls', 'Coorg', 60.0, 10.0, 'Kumaradhara', 'Somwarpet', 'Kodagu', 10.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(14, 'Chunchanakatte Falls', 'Krishnarajanagara', 20.0, 10.0, 'Kaveri', 'Krishnarajanagara', 'Mysore', 10.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(15, 'Jenukallu Falls', 'Chitradurga', 60.0, 10.0, 'Tungabhadra', 'Hosadurga', 'Chitradurga', 10.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(16, 'Bennethora Falls', 'Sirsi', 300.0, 10.0, 'Aghanashini', 'Sirsi', 'Uttara Kannada', 20.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(17, 'Hidlumane Falls', 'Kundapura', 150.0, 10.0, 'Varahi', 'Kundapura', 'Udupi', 20.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(18, 'Hanumangundi Falls', 'Kudremukh', 22.0, 10.0, 'Bhadra', 'Kudremukh', 'Chikkamagaluru', 20.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(19, 'Shanthi Falls', 'Kudremukh', 20.0, 10.0, 'Bhadra', 'Kudremukh', 'Chikkamagaluru', 20.0, 'Monsoon');
+INSERT INTO karnataka_waterfalls VALUES(20, 'Sirimane Falls', 'Sringeri', 40.0, 10.0, 'Tunga', 'Sringeri', 'Chikkamagaluru', 10.0, 'Monsoon');
+
+
+
+
+
+
